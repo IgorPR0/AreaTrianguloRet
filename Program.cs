ab7a502ae9e1dfcb5a85f908b30771d5962baf3f
IgorPR0 <igortecnologia2020@gmail.com>
2020-09-30 11:56:13 -0300

    Feito atividade AreaTrianguloRet

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        double Base;
+        double altura;
+        double area;
+        
+        Console.Write("Digite o valor da base: ");                   
+        Base = double.Parse(Console.ReadLine());
+        
+        Console.Write("Digite o valor de altura: ");
+        altura = double.Parse(Console.ReadLine());
+
+        area = Base * altura /2; 
+
+        Console.WriteLine("Resultado da area é: ");
+        Console.WriteLine(area);
+        Console.ReadKey();   
+        
+        }
+    }
+}
